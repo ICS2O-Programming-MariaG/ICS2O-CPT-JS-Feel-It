8f94e424fe83691c4c1c4eb3cd58288ecf3f3d23
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-30 21:20:21 +0000

    Set JS code for changing background size - finished CPT-02

--- js/game.js
+++ js/game.js
@@ -3,6 +3,7 @@
 // Created by: Maria G
 // Created on: May 30, 2022
 // This file contains the JS code for index.html
+// Phaser 3 configuration file
 
 /* global Phaser */
 
@@ -13,7 +14,21 @@ const CONFIG = {
   //dimensions of screen
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  //setup so default arcade games can be played
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    },
+  },
+  //settings for background
+  backgroundColor: 0xBCDADE,
+  //making scale of background change when screen/window size changes
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //centring the background on the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const GAME = new Phaser.Game(CONFIG);
