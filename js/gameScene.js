6532d351a3170cf7c9bbb591fb378d568bd779e3
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-06-01 00:28:14 +0000

    Added bee sprite and background image to gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -9,8 +9,14 @@
 //code written below extends (adds to) prewritten code in Phaser.Scene
 class gameScene extends Phaser.Scene {
   constructor() {
-    //"super" runs Phaser.Scene first
+    //"super" accesses the properties of Phaser first
     super({key: 'gameScene'});
+
+    //initializing the background variable
+    this.gameSceneBackground = null;
+
+    //initializing the bee sprite variable
+    this.beeSprite = null;
   }
 
   init(data) {
@@ -21,10 +27,22 @@ class gameScene extends Phaser.Scene {
   preload() {
     //for debugging purposes: writes Splash Scene in the console to let programmer know what scene/file is being displayed
     console.log("Game Scene");
+
+    //loading the background image
+    this.load.image('skyBackground', '../images/gameSceneBackground.png');
+
+    //loading the bee sprite image
+    this.load.image('beeSprite', '../images/beeSprite.png');
   }
 
   create(data) {
-    //come back to later
+    //creating the background to display to the screen
+    this.gameSceneBackground = this.add.image(0, 0, 'skyBackground');
+    //positioning the background image to take up the screen
+    this.gameSceneBackground.setOrigin(0, 0);
+
+    //creating the bee sprite on the screen
+    this.beeSprite = this.physics.add.sprite(100, 1080 / 2, 'beeSprite').setScale(0.25);
   }
 
   update(time, delta) {
